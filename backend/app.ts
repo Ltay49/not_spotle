57f724d03f45ced65f8410e0f671268503c1a655
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-02-05 00:17:46 +0000

    cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import {getApi, getStats, getPlayer, postChosenPlayer} from './controllers/app.controller'
+import cors from "cors";
 const app = express();
+
+app.use(cors());
 
 // Define a basic route to keep the server alive
 app.get("/api", getApi)
