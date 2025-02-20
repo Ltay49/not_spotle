30c47b88ec4fea5ba5170d133752510a2662bed0
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-02-20 17:33:04 +0000

    guess functionality improved

--- barclaysmen/app/login.tsx
+++ barclaysmen/app/login.tsx
@@ -1,109 +1,112 @@
-import { View, Text, TouchableOpacity, StyleSheet, TextInput, ImageBackground } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground, ActivityIndicator } from "react-native";
 import { useState } from "react";
 import { useFonts, Chewy_400Regular } from '@expo-google-fonts/chewy';
-const Lua = require('./../assets/images/LuaLua.png'); 
 import { LuckiestGuy_400Regular } from '@expo-google-fonts/luckiest-guy';
-import Game from './screens/game/game'
+import Game from './screens/game/game';
+
+const Lua = require('./../assets/images/LuaLua.png'); 
 
 export default function Login() {
-    const [gameStart, setGameStart] = useState<boolean | null>(null); 
-    // Initially null to avoid showing a message immediately
+    const [gameStart, setGameStart] = useState<boolean | null>(null); // Initially null to avoid showing a message immediately
     const [fontsLoaded] = useFonts({
-      Chewy_400Regular,
-      LuckiestGuy_400Regular,
-  });
+        Chewy_400Regular,
+        LuckiestGuy_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="blue" />
+            </View>
+        );
+    }
+
     if (gameStart === true) {
         return <Game />;
-      }
+    }
+
     const handleSubmit = (): void => {
-    setGameStart(true)
+        setGameStart(true);
     };
 
-  return (
-    <ImageBackground source={Lua} style={styles.background} resizeMode="cover" blurRadius={5}>
-    <View style={styles.overlay}> 
-<View style={styles.container}>
-  <View style={styles.innerContainer}>
-    <View style={styles.inputContainer}>
-      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
-        <Text style={styles.buttonText}>Play</Text>
-      </TouchableOpacity>
-     
-    </View>
-    </View>
-    </View>
-    </View>
-    </ImageBackground>
-  );
+    return (
+        <ImageBackground source={Lua} style={styles.background} resizeMode="cover" blurRadius={5}>
+            <View style={styles.overlay}> 
+                <View style={styles.container}>
+                    <View style={styles.innerContainer}>
+                        <View style={styles.inputContainer}>
+                            <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+                                <Text style={styles.buttonText}>Play</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </ImageBackground>
+    );
 }
+
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-},
-overlay: {
-  flex: 1,
-  width: '100%',
-  backgroundColor: 'rgba(255, 255, 255, 0.7)', // Optional: Add a translucent overlay
-  justifyContent: 'center',
-  alignItems: 'center',
-},
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
+    background: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    overlay: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: 'rgba(255, 255, 255, 0.7)', // Optional: Add a translucent overlay
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     container: {
-      flex: 1,
-      borderWidth: 2,
-      justifyContent: "center",
-      borderRadius: 10,
-      width:340
-  },
-  innerContainer: {
-       borderWidth: 2,
-      // flex: 1, 
-      justifyContent: "center",
-      
-  },
+        flex: 1,
+        borderWidth: 2,
+        justifyContent: "center",
+        borderRadius: 10,
+        width: 340,
+    },
+    innerContainer: {
+        borderWidth: 2,
+        justifyContent: "center",
+    },
     inputContainer: {
-      // flex: 1, 
-      width: '50%',
-      padding: 20,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 10,
-      backgroundColor: 'black',
-      shadowColor: '#000',
-      shadowOpacity: 0.2,
-      shadowOffset: { width: 0, height: 5 },
-      shadowRadius: 4,
-      elevation: 3,
-      alignSelf:'center',
-      justifyContent: "center",
+        width: '50%',
+        padding: 20,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        backgroundColor: 'black',
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 5 },
+        shadowRadius: 4,
+        elevation: 3,
+        alignSelf: 'center',
+        justifyContent: "center",
     },
     button: {
-      justifyContent: "center",
-      width:'100%',
-      borderWidth:1,
-      borderColor:'white',
-      backgroundColor: 'black',
-      padding: 10,
-      borderRadius: 5,
-      alignItems: 'center',
+        justifyContent: "center",
+        width: '100%',
+        borderWidth: 1,
+        borderColor: 'white',
+        backgroundColor: 'black',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
     },
     buttonText: {
-      // borderColor:'white',
-      // borderWidth:1,
-      fontSize:22,
-      fontFamily: 'LuckiestGuy_400Regular',
-      color: 'lightblue',
-      alignContent:'center',
-      marginTop:8
-
+        fontSize: 22,
+        fontFamily: 'LuckiestGuy_400Regular',
+        color: 'lightblue',
+        marginTop: 8,
     },
-  });
-  
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+});
